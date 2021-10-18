8d467b8303e03d32acfa95638e3dd40db9cba335
fsoysal462 <fsoysal462@yandex.ru>
2021-10-19 00:14:53 +0300

    18 Ekim Dersi

---
+++ src/app.js
@@ -0,0 +1,32 @@
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+
+//array
+//camelCasing
+//PascalCasing
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi","Emlak konut kredisi","Kamu Konut Kredisi"]
+
+for (let i = 0; i<konutKredileri.length; i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
+console.log("<ul>")
+{/* <ul>  
+    <li>İhtiyaç Kredisi</li>
+    <li>Taşıt Kredisi</li>
+    <li>Konut Kredisi</li>
+</ul>*/}
+
+console.log(konutKredileri)
